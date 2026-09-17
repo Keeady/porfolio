b7b94ecc622faa61dae2cc373a4a681c47f3283e
Camaria Bevavy <cabevavy@microsoft.com>
2026-09-16 21:35:24 -0700

    add missing pages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import Header from "./components/header";
 import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <>
-    <Header />
     <Hero />
-    </>
   )
 }

b3df30faff9678ff32f4a89b2e819827e667f79b
Camaria Bevavy <cabevavy@microsoft.com>
2026-09-18 23:55:02 -0700

    add deps

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
 from api.projects import get_projects
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
 
 @app.get("/projects")
 async def project_list():

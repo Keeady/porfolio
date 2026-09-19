60507352897d0dfe0f45f6f32143931fc9abd97a
Camaria Bevavy <cabevavy@microsoft.com>
2026-09-18 22:41:36 -0700

    add list item

--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -7,8 +7,8 @@ type ProjectItem = {
   subtitle: string;
   description: string;
   skills: string[];
-  imageSrc: "";
-  companyIcon: "";
+  imageSrc: string;
+  companyIcon: string;
 };
 
 export default async function Projects() {
@@ -18,15 +18,17 @@ export default async function Projects() {
     <section className="relative flex flex-col items-center overflow-hidden bg-[#040B14] px-6 pb-24 pt-20 text-center">
       <ul>
         {projects.map((p: ProjectItem) => (
-          <ProjectCard
-            key={p.id}
-            title={p.title}
-            subtitle={p.subtitle}
-            description={p.description}
-            skills={p.skills}
-            companyIcon={p.companyIcon}
-            imageSrc={p.imageSrc}
-          />
+          <li key={p.id}>
+            <ProjectCard
+              key={p.id}
+              title={p.title}
+              subtitle={p.subtitle}
+              description={p.description}
+              skills={p.skills}
+              companyIcon={p.companyIcon}
+              imageSrc={p.imageSrc}
+            />
+          </li>
         ))}
       </ul>
     </section>

17b6f21a600a1fc7d67e2b21cae0688eef223685
Camaria Bevavy <cabevavy@microsoft.com>
2026-09-20 00:23:34 -0700

    remove console

--- frontend/app/bio/page.tsx
+++ frontend/app/bio/page.tsx
@@ -10,7 +10,6 @@ type BioItem = {
 
 export default async function Bio() {
   const bioItems = await getBio();
-  console.log(bioItems)
 
   return (
     <section className="relative flex flex-col items-center overflow-hidden bg-[#040B14] px-6 pb-24 pt-20 text-center">
